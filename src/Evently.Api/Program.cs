69249bd513f22fc33b14e1089f223e376b239781
Otávio Odenik <otavio.odenik@etec.sp.gov.br>
2026-03-11 12:31:22 -0300

    feat: Adds initial project structure
    Adds database and docker support

--- src/Evently.Api/Program.cs
+++ src/Evently.Api/Program.cs
@@ -1,13 +1,25 @@
+using Evently.Api.Extensions;
+using Evently.Modules.Events.Api;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddEventsModule(builder.Configuration);
 
 WebApplication app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    
+    app.ApplyMigrations();
 }
+
+EventsModule.MapEndpoints(app);
 
 app.Run();
 
