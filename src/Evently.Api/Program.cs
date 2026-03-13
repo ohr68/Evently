9c9d1c2d5eacef2dd3e07dd03a7cb41e33f8eed5
Otávio Odenik <otavio.odenik@etec.sp.gov.br>
2026-03-13 11:31:02 -0300

    feat: Adds structured logging with Seq and Serilog

--- src/Evently.Api/Program.cs
+++ src/Evently.Api/Program.cs
@@ -3,8 +3,11 @@ using Evently.Common.Application;
 using Evently.Common.Infrastructure;
 using Evently.Modules.Events.Application;
 using Evently.Modules.Events.Infrastructure;
+using Serilog;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, loggerConfig) => loggerConfig.ReadFrom.Configuration(context.Configuration));
 
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen(options =>
@@ -29,5 +32,7 @@ if (app.Environment.IsDevelopment())
 }
 
 EventsModule.MapEndpoints(app);
+
+app.UseSerilogRequestLogging();
 
 app.Run();
